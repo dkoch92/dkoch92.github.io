6195240ceaa09750b4b2755c9468780c985f68bd
Daniel <37514699+dkoch92@users.noreply.github.com>
2018-05-16 16:15:58 -0400

    Add files via upload

---
+++ read_csv.js
@@ -0,0 +1,14 @@
+function ParseData(){
+	Papa.parse("test_stats2.csv", {
+		download: true,
+		header: false,
+		dynamicTyping: true,
+		complete: function(results) {
+			console.log("Parsed The Data!"),
+			console.log(results)
+		}
+	})
+}
+
+
+ParseData()
