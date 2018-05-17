4d954bea4e8f4b6ba2f503eba697dbd7d94568bb
Daniel <37514699+dkoch92@users.noreply.github.com>
2018-05-17 12:23:42 -0400

    Add files via upload

--- read_csv.js
+++ read_csv.js
@@ -1,4 +1,4 @@
-function ParseData(){
+function ParseData( CallBack ){
 	var parsed_data = [
 		[99,	1,	4,	0,	0,	0,	0,	0,	0,	0,	0,	0],
 		[98,	2,	3,	0,	0,	0,	0,	0,	0,	0,	0,	0],
@@ -12,9 +12,7 @@ function ParseData(){
 		[0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0],
 		[0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0],
 		[0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0]]
-	console.log( parsed_data )
-	console.log( parsed_data[0] )
-	console.log( parsed_data[0][0] )
+
 	Papa.parse("test_stats2.csv", {
 		download: true,
 		header: false,
@@ -25,21 +23,33 @@ function ParseData(){
 			for (var i = 0; i < 12; i++) {
 				for (var j = 0; j < 12; j++) {
 					parsed_data[i][j] =  results.data[i][j]
-					console.log( results.data[i][j] )
 				}
 			}
-		console.log( parsed_data )
-		console.log( parsed_data[0] )
-		console.log( parsed_data[0][0] )
+			console.log( parsed_data )
+			console.log( parsed_data[0] )
+			console.log( parsed_data[0][0] )
+			CallBack( results.data )
+
 		}
 	})
-	console.log( parsed_data )
-	console.log( parsed_data[0] )
-	console.log( parsed_data[0][0] )
-	return parsed_data
 }
 
 var TestWeek = [
+[29,	13,	38,	3,	0.3256,	0.4474,	71,	5,	1,	3.309,	1.162,	9.397],
+[34,	12,	40,	6,	0.3603,	0.4731,	63,	6,	5,	3.068,	1.023,	7.732],
+[50,	18,	36,	3,	0.3397,	0.5144,	50,	2,	0,	4.333,	1.204,	8.333],
+[45,	9,	39,	5,	0.3242,	0.4515,	83,	8,	2,	3.054,	1.208,	10.14],
+[35,	6,	23,	11,	0.3412,	0.4318,	66,	5,	6,	4.655,	1.256,	8.778],
+[39,	15,	50,	2,	0.3500,	0.5131,	53,	6,	0,	2.714,	1.206,	7.571],
+[48,	9,	30,	3,	0.3271,	0.4007,	76,	5,	4,	4.817,	1.183,	9.634],
+[39,	18,	46,	10,	0.3908,	0.6008,	69,	5,	4,	3.240,	1.320,	9.315],
+[49,	19,	50,	4,	0.4050,	0.6612,	49,	1,	3,	3.942,	1.161,	9.657],
+[42,	13,	47,	5,	0.3711,	0.4900,	55,	3,	5,	2.732,	1.196,	8.839],
+[43,	14,	43,	7,	0.3583,	0.5148,	59,	2,	2,	5.072,	1.740,	8.801],
+[49,	15,	56,	8,	0.3705,	0.5438,	55,	3,	7,	5.050,	1.317,	10.683]
+]
+
+var Test2 = [
 [29,	13,	38,	3,	0.3256,	0.4474,	71,	5,	1,	3.309,	1.162,	9.397],
 [34,	12,	40,	6,	0.3603,	0.4731,	63,	6,	5,	3.068,	1.023,	7.732],
 [50,	18,	36,	3,	0.3397,	0.5144,	50,	2,	0,	4.333,	1.204,	8.333],
@@ -61,7 +71,25 @@ function DisplayData( data ){
 	console.log( data[0][0] )
 }
 
-var Data = ParseData()
+function SetValues( data) {
+	for (var i = 0; i < 12; i++) {
+		for (var j = 0; j < 12; j++) {
+			Test2[i][j] =  data[i][j]
+		}
+	}
+}
+
+
+Papa.parsePromise = function(file) {
+  return new Promise(function(complete, error) {
+    Papa.parse(file, {complete, error});
+  });
+};
+
+Papa.parsePromise("test_stats2.csv") .
+  then(function(results) { console.log(results); });
+
+//ParseData( SetValues )
 
 //console.log('Data:')
 //console.log( Data )
