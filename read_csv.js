93c7816320a179fd879bf2abbda3cf9bd6da280b
Daniel <37514699+dkoch92@users.noreply.github.com>
2018-05-17 09:24:36 -0400

    Add files via upload

--- read_csv.js
+++ read_csv.js
@@ -1,7 +1,3 @@
-function SetData( stats ){
-	Stats = stats
-}
-
 function ParseData(){
 	var parsed_data = []
 	Papa.parse("test_stats2.csv", {
@@ -11,7 +7,6 @@ function ParseData(){
 		complete: function(results) {
 			console.log("Parsed The Data!"),
 			console.log(results.data)
-			SetData( results.data )
 			for (var i = 0; i < 12; i++) {
 				row = []
 				for (var j = 0; j < 12; j++) {
@@ -30,6 +25,12 @@ var Data = ParseData()
 
 console.log('Data:')
 console.log(Data)
+console.log(Data[0])
+console.log(Data[0][0])
+console.log(Data[11])
 
-console.log('Stats:')
-console.log(Stats)
+for( var i =0; i < 12; i++ ){
+	console.log( Data[3][i] )
+}
+
+
