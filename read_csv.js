5c18859ca98a6dc134fa11c3bcb3cd8c4a1fbc2d
Daniel <37514699+dkoch92@users.noreply.github.com>
2018-05-17 17:07:17 -0400

    Add files via upload

--- read_csv.js
+++ read_csv.js
@@ -1,38 +1,5 @@
 week = 6
 
-function ParseData( CallBack, week ){
-	for(var i = 0; i < week; i++ ){
-		file = "week_"+(i+1).toString()+"_stats.csv"
-		Papa.parse(file, {
-			download: true,
-			header: false,
-			dynamicTyping: true,
-			complete: function(results) {
-				CallBack( results.data,i )
-			}
-		})
-	}
-}
-
-
-function CollectData( data,week ) {
-	for (var i = 0; i < 12; i++) {
-		for (var j = 0; j < 12; j++) {
-			GetWeek(week)[i][j] =  data[i][j]
-		}
-	}
-}
-
-
-function TestButton() {
-	console.log('Button Works')
-	console.log( Week4 )
-}
-
-function NowParse() {
-	ParseData( CollectData,week )
-}
-
 var Blank = [
 [0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0],
 [0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0],
@@ -55,59 +22,80 @@ var Week5
 var Week6
 
 
-CreateMatrices(week,Blank)
-
-
-//console.log('Data:')
-//console.log( Data )
-//console.log( TestWeek )
-
-//DisplayData( Data )
-//DisplayData( TestWeek )
-
-
-function GetWeek(index){
-	if( index === 0){
-		return Week1
+function ParseData( CallBack, Create, week, blank ){
+	for(var i = 0; i < week; i++ ){
+		file = "week_"+(i+1).toString()+"_stats.csv"
+		Papa.parse(file, {
+			download: true,
+			header: false,
+			dynamicTyping: true,
+			complete: function(results) {
+				Create( i,blank )
+				CallBack( results.data,i,blank )
+			}
+		})
 	}
-	if( index === 1){
-		return Week2
-	}
-	if( index === 2){
-		return Week3
-	}
-	if( index === 3){
-		return Week4
-	}
-	if( index === 4){
-		return Week5
-	}
-	if( index === 5){
-		return Week6
-	}	
-
 }
 
-function CreateMatrices(week,blank){
-	for(var i = 0; i < week; i++){
-		if( i === 0){
-			Week1 = blank
+
+function CollectData( data,week,mat ) {
+	for (var i = 0; i < 12; i++) {
+		for (var j = 0; j < 12; j++) {
+			mat[i][j] =  data[i][j]
 		}
-		if( i === 1){
-			Week2 = blank
-		}
-		if( i === 2){
-			Week3 = blank
-		}
-		if( i === 3){
-			Week4 = blank
-		}
-		if( i === 4){
-			Week5 = blank
-		}
-		if( i === 5){
-			Week6 = blank
-		}
+	}
+	console.log(mat)
+	SetToWeek(mat,week)
+}
+
+function SetToWeek(M,W){
+	if( W === 0 ){
+		Week1 = M
+	}
+	if( W === 1 ){
+		Week2 = M
+	}
+	if( W === 2 ){
+		Week3 = M
+	}
+	if( W === 3 ){
+		Week4 = M
+	}
+	if( W === 4 ){
+		Week5 = M
+	}
+	if( W === 5 ){
+		Week6 = M
+	}
+}
+
+function TestButton() {
+	console.log('Button Works')
+	console.log( Week4 )
+}
+
+function NowParse() {
+	ParseData( CollectData,CreateMatrix,week,Blank )
+}
+
+function CreateMatrix(week_index,blank){
+	if( week_index === 0){
+		Week1 = blank
+	}
+	if( week_index === 1){
+		Week2 = blank
+	}
+	if( week_index === 2){
+		Week3 = blank
+	}
+	if( week_index === 3){
+		Week4 = blank
+	}
+	if( week_index === 4){
+		Week5 = blank
+	}
+	if( week_index === 5){
+		Week6 = blank
 	}
 }
 
