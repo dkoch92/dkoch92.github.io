66ab70b6b47386f3e71843785835ef5b4b3bcc4e
Daniel <37514699+dkoch92@users.noreply.github.com>
2018-05-24 19:33:59 -0400

    Add files via upload

---
+++ Display_Leaders.js
@@ -0,0 +1,132 @@
+// Creating Empty Matrices
+//----------------------------------------------------------------
+Test_Leaders = [
+['Nate','Clint','Nate'],				
+[55,53,52]	,		
+['Allen','Will','Josh','Will','Nate','Brian']	,
+[19,	18, '  ',	  	17	 ,'  ','  '	 ]		,	
+['Daniel',	'Daniel',	'Clint',	'Will',	'Daniel'	,'Mike',	'Charlie',	'Allen'	,'Matt'],
+[56,	54, '  ',	 	50,'  ','  ','  ','  ','  ']	, 	 	 	 	 
+['Will',	'McD',	'McD',	'Josh',	'Cullen',	'Nate']	,		
+[13,	11,	10,'  ','  ','  '],
+['Brian',	'Allen',	'Will'],
+[0.4188,0.405,	0.4],
+['Allen',	'Josh',	'Will'],
+[0.6612,	0.6008,	0.5797]
+] 	
+
+Test_Places = [
+[1,1,1],
+[1,2,3],
+[1,2,6],
+[1,1,4],
+[1,1,1],
+[1,1,1]] 	 			
+
+
+
+// Setting all the Initial Components
+//----------------------------------------------------------------
+
+var Leader_Pics = ['L_1.png','L_2.png','L_3.png','L_4.png','L_5.png','L_6.png']
+var Leader_Left = ['0px','168px','336px', '504px','672px','840px']
+
+var H_id = [['H1_1','H1_2','H1_3'],['H2_1','H2_2','H2_3'],['H3_1','H3_2','H3_3'],['H4_1','H4_2','H4_3'],['H5_1','H5_2','H5_3'],['H6_1','H6_2','H6_3']]
+var h_id = [['h1_1','h1_2','h1_3'],['h2_1','h2_2','h2_3'],['h3_1','h3_2','h3_3'],['h4_1','h4_2','h4_3'],['h5_1','h5_2','h5_3'],['h6_1','h6_2','h6_3']]
+
+var P_id = [['P1_1','P1_2','P1_3'],['P2_1','P2_2','P2_3'],['P3_1','P3_2','P3_3'],['P4_1','P4_2','P4_3'],['P5_1','P5_2','P5_3'],['P6_1','P6_2','P6_3']]
+var p_id = [['p1_1','p1_2','p1_3'],['p2_1','p2_2','p2_3'],['p3_1','p3_2','p3_3'],['p4_1','p4_2','p4_3'],['p5_1','p5_2','p5_3'],['p6_1','p6_2','p6_3']]
+
+var place_color = ['rgba(212,175,55,.45)','rgba(192,192,192,.45)','rgba(205,127,50,.45)']
+
+var H_array = CreateTextId('H')
+var P_array = CreateTextId('P')
+
+var AllWeeks_L
+
+setTimeout(function(){ 
+	AllWeeks_L = GetAllWeeks_L()
+	console.log(ALLWeeks_L)
+	Test_Text_Display(AllWeeks_L[0],H_array)
+	Test_H_Display();
+    Test_P_Display();
+	Test_Text_Display()}, 400)
+
+
+// Defining Functions
+//-----------------------------------------------------------------
+
+function Test_Text_Display(lead,id){
+	for(var i = 0; i < 12; i++){
+		for(var j = 0; j < lead[i].length ;j++){
+			text_tag = id[i][j]
+			document.getElementById(text_tag).innerHTML = lead[i][j]
+		}
+	}
+}
+
+function CreateTextId(letter){
+	var id_array = []
+	for(var i = 1; i < 7;i++){
+		for(var j = 1; j < 3;j++){
+			row = []
+			for(var k = 1; k < 10; k++){
+				row.push( 'text_'+letter+i.toString()+k.toString()+'_'+j.toString() )
+			}
+			id_array.push( row )
+		}
+	}
+	return id_array
+}
+
+function Test_P_Display(){
+	for(var i = 0; i < 6;i++){
+		var total_length = 0
+		var total_top = 0
+		for(var j = 0; j < 3; j++){
+			length = Test_Places[i][j]
+			top_px = total_top.toString()+'px'
+			total_length = total_length + length
+			cell_height = (length*36).toString()+'px'
+			total_top = total_top + length*36
+			cell_tag = P_id[i][j]
+			cell_child_tag = cell_tag+'_color'
+			img_tag = p_id[i][j]
+			document.getElementById(cell_tag).style.height = cell_height
+			document.getElementById(cell_tag).style.top = top_px
+			document.getElementById(cell_tag).style.left = Leader_Left[i]
+			document.getElementById(cell_tag).style.background = place_color[j]
+			document.getElementById(cell_child_tag).style.position = 'absolute'
+			document.getElementById(cell_child_tag).style.height = '100%'
+			document.getElementById(cell_child_tag).style.width = '100%'
+			document.getElementById(cell_child_tag).style.background = place_color[j]
+			document.getElementById(img_tag).src = Leader_Pics[length-1]
+		}
+	}
+}
+
+function Test_H_Display(){
+	for(var i = 0; i < 6;i++){
+		var total_length = 0
+		var total_top = 0
+		for(var j = 0; j < 3; j++){
+			length = Test_Places[i][j]
+			top_px = total_top.toString()+'px'
+			total_length = total_length + length
+			cell_height = (length*36).toString()+'px'
+			total_top = total_top + length*36
+			cell_tag = H_id[i][j]
+			cell_child_tag = cell_tag+'_color'
+			img_tag = h_id[i][j]
+			document.getElementById(cell_tag).style.height = cell_height
+			document.getElementById(cell_tag).style.top = top_px
+			document.getElementById(cell_tag).style.left = Leader_Left[i]
+			document.getElementById(cell_tag).style.background = place_color[j]
+			document.getElementById(cell_child_tag).style.position = 'absolute'
+			document.getElementById(cell_child_tag).style.height = '100%'
+			document.getElementById(cell_child_tag).style.width = '100%'
+			document.getElementById(cell_child_tag).style.background = place_color[j]
+			document.getElementById(img_tag).src = Leader_Pics[length-1]
+		}
+	}
+}
---
+++ Read_Leaders.js
@@ -0,0 +1,209 @@
+var Week1_L = []
+var Week2_L = []
+var Week3_L = []
+var Week4_L = []
+var Week5_L = []
+var Week6_L = []
+var Week7_L = []
+var Week8_L = []
+var Week9_L = []
+var Week10_L = []
+var Week11_L = []
+var Week12_L = []
+var Week13_L = []
+var Week14_L = []
+var Week15_L = []
+var Week16_L = []
+var Week17_L = []
+var Week18_L = []
+var Week19_L = []
+var Week20_L = []
+var Season_L = []
+
+//  Setting all the Initial Components
+//----------------------------------------------------------------
+var week = 7
+
+var CSV_Names_L = ["week_1_leaders.csv","week_2_leaders.csv","week_3_leaders.csv","week_4_leaders.csv","week_5_leaders.csv",
+				   "week_6_leaders.csv","week_7_leaders.csv","week_8_leaders.csv","week_9_leaders.csv","week_10_leaders.csv",
+				   "week_11_leaders.csv","week_12_leaders.csv","week_13_leaders.csv","week_14_leaders.csv","week_15_leaders.csv",
+				   "week_16_leaders.csv","week_17_leaders.csv","week_18_leaders.csv","week_19_leaders.csv","week_20_leaders.csv"]
+
+var AllWeeks_L
+
+setTimeout(function(){ 
+	ParseAll_L(week,CSV_Names_L);},100)
+
+setTimeout(function(){ 	
+	console.log(Week2_L);
+	AllWeeks_L = CombineAllWeeks_L( week );
+	console.log(AllWeeks_L);
+	console.log('Finished Parsing Leaders');},200)
+
+// Defining Functions
+//-----------------------------------------------------------------
+
+function GetAllWeeks_L(){
+	return AllWeeks_L
+}
+
+function ParseAll_L( week,names ){
+	for( var i = 0; i < week; i++ ){
+		NowParse_L(i,names)
+	}
+}
+
+function ParseData_L( CallBack, file, Week_Mat){
+	Papa.parse(file, {
+		download: true,
+		header: false,
+		dynamicTyping: true,
+		complete: function(results) {
+			CallBack(results.data,Week_Mat)
+		}
+	})
+}
+
+function CollectData_L( data, mat ) {
+	for (var i = 0; i < 24; i++) {
+		mat.push( data[i] )
+	}
+}
+
+function GetWeekNumber_L(){
+	return week
+}
+
+function NowParse_L(index,file) {
+	name = file[index]
+	filename = '../CSV Files/'+name
+	console.log(filename)
+	if(index === 0){
+		ParseData_L( CollectData_L,filename,Week1_L)
+	}
+	if(index === 1){
+		ParseData_L( CollectData_L,filename,Week2_L)
+	}
+	if(index === 2){
+		ParseData_L( CollectData_L,filename,Week3_L)
+	}
+	if(index === 3){
+		ParseData_L( CollectData_L,filename,Week4_L)
+	}
+	if(index === 4){
+		ParseData_L( CollectData_L,filename,Week5_L)
+	}	
+	if(index === 5){
+		ParseData_L( CollectData_L,filename,Week6_L)
+	}
+	if(index === 6){
+		ParseData_L( CollectData_L,filename,Week7_L)
+	}
+	if(index === 7){
+		ParseData_L( CollectData_L,filename,Week8_L)
+	}
+	if(index === 8){
+		ParseData_L( CollectData_L,filename,Week9_L)
+	}
+	if(index === 9){
+		ParseData_L( CollectData_L,filename,Week10_L)
+	}
+	if(index === 10){
+		ParseData_L( CollectData_L,filename,Week11_L)
+	}
+	if(index === 11){
+		ParseData_L( CollectData_L,filename,Week12_L)
+	}
+	if(index === 12){
+		ParseData_L( CollectData_L,filename,Week13_L)
+	}
+	if(index === 13){
+		ParseData_L( CollectData_L,filename,Week14_L)
+	}
+	if(index === 14){
+		ParseData_L( CollectData_L,filename,Week15_L)
+	}	
+	if(index === 15){
+		ParseData_L( CollectData_L,filename,Week16_L)
+	}
+	if(index === 16){
+		ParseData_L( CollectData_L,filename,Week17_L)
+	}
+	if(index === 17){
+		ParseData_L( CollectData_L,filename,Week18_L)
+	}
+	if(index === 18){
+		ParseData_L( CollectData_L,filename,Week19_L)
+	}
+	if(index === 19){
+		ParseData_L( CollectData_L,filename,Week20_L)
+	}
+	
+}
+
+function CombineAllWeeks_L(week){
+	var AllWeeks = []
+	for( var i = 0; i < week ;i++ ){
+		if(i==0){
+			AllWeeks.push(Week1_L)
+		}
+		if(i==1){
+			AllWeeks.push(Week2_L)
+		}
+		if(i==2){
+			AllWeeks.push(Week3_L)
+		}
+		if(i==3){
+			AllWeeks.push(Week4_L)
+		}
+		if(i==4){
+			AllWeeks.push(Week5_L)
+		}
+		if(i==5){
+			AllWeeks.push(Week6_L)
+		}
+		if(i==6){
+			AllWeeks.push(Week7_L)
+		}
+		if(i==7){
+			AllWeeks.push(Week8_L)
+		}
+		if(i==8){
+			AllWeeks.push(Week9_L)
+		}
+		if(i==9){
+			AllWeeks.push(Week10_L)
+		}
+		if(i==10){
+			AllWeeks.push(Week11_L)
+		}
+		if(i==11){
+			AllWeeks.push(Week12_L)
+		}
+		if(i==12){
+			AllWeeks.push(Week13_L)
+		}
+		if(i==13){
+			AllWeeks.push(Week14_L)
+		}
+		if(i==14){
+			AllWeeks.push(Week15_L)
+		}
+		if(i==15){
+			AllWeeks.push(Week16_L)
+		}
+		if(i==16){
+			AllWeeks.push(Week17_L)
+		}
+		if(i==17){
+			AllWeeks.push(Week18_L)
+		}
+		if(i==18){
+			AllWeeks.push(Week19_L)
+		}
+		if(i==19){
+			AllWeeks.push(Week20_L)
+		}
+	}
+	return AllWeeks
+}
