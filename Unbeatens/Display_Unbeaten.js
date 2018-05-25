2fbe170421392dd0d094082d9260e4f8aa695b52
Daniel <37514699+dkoch92@users.noreply.github.com>
2018-05-25 18:01:06 -0400

    Add files via upload

--- Unbeatens/Display_Unbeaten.js
+++ Unbeatens/Display_Unbeaten.js
@@ -3,6 +3,10 @@
 var U_id = U_CreateId('U')
 var B_id = U_CreateId('B')
 
+var Week_Titles = ['Week 1','Week 2','Week 3','Week 4','Week 5','Week 6','Week 7','Week 8','Week 9','Week 10'
+                  ,'Week 11','Week 12','Week 13','Week 14','Week 15','Week 16','Week 17','Week 18','Week 19','Week 20']
+
+
 var week
 var All_U
 var All_B
@@ -10,13 +14,13 @@ var All_B
 setTimeout(function(){ 
 	week = GetWeekNumber_U();
 	All_U = GetAllWeeks_U();
-	All_B = GetAllWeeks_B();
-	console.log('All U',All_U);
-	console.log('All U',All_U);}, 400)
+	All_B = GetAllWeeks_B();}, 400)
 
 setTimeout(function(){ 
-	U_DisplayColors(All_U[4],U_id);
-	B_DisplayColors(All_B[4],B_id);}, 500)
+	U_DisplayColors(All_U[week-1],U_id);
+	B_DisplayColors(All_B[week-1],B_id);
+	DisplayWeekTitle(week,'U');
+	DisplayWeekTitle(week,'B')}, 500)
 
 
 
@@ -33,6 +37,12 @@ function U_CreateId(letter){
 		id_array.push(row)
 	}
 	return id_array
+}
+
+function DisplayWeekTitle(w,letter){
+	title_tag = letter+'_title'
+	title_string = Week_Titles[w-1]
+	document.getElementById(title_tag).innerHTML = title_string
 }
 
 function U_DisplayColors(Mat,id){
@@ -60,5 +70,27 @@ function B_DisplayColors(Mat,id){
 			}
 			document.getElementById(stat_tag).style.background = rbga_color
 		}
+	}
+}
+
+function DisplayWeek_U(w){
+	if(w <= week){
+		U_DisplayColors(All_U[w-1],U_id)
+		DisplayWeekTitle(w,'U')
+	}
+	else{
+		U_DisplayColors(All_U[week-1],U_id)
+		DisplayWeekTitle(week-1,'U')
+	}
+}
+
+function DisplayWeek_B(w){
+	if(w <= week){
+		U_DisplayColors(All_B[w-1],B_id)
+		DisplayWeekTitle(w,'B')
+	}
+	else{
+		U_DisplayColors(All_B[week-1],B_id)
+		DisplayWeekTitle(week-1,'B')
 	}
 }
