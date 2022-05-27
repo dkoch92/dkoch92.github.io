17b0d1e4ed164685f7bc7bba1a0835e1548de27d
Daniel <37514699+dkoch92@users.noreply.github.com>
2022-05-27 10:47:27 -0400

    Add files via upload

---
+++ Season_22/Leaders/Display_Leaders.js
@@ -0,0 +1,153 @@
+// Setting all the Initial Components
+//----------------------------------------------------------------
+
+var Leader_Pics = ['L_1.png','L_2.png','L_3.png','L_4.png','L_5.png','L_6.png']
+var Leader_Left = ['0px','168px','336px', '504px','672px','840px']
+
+var TeamPics = ['Will Pic.png','Clint Pic.png','Brian Pic.png','Daniel Pic.png','Mike Pic.png','Cullen Pic.png','Ryan Pic.png',
+				'Charlie Pic.png','Allen Pic.png','Josh Pic.png','Matt Pic.png','McD Pic.png','Nate Pic.png','Tim Pic.png']
+
+var Week_Titles = ['Week 1','Week 2','Week 3','Week 4','Week 5','Week 6','Week 7','Week 8','Week 9','Week 10'
+                  ,'Week 11','Week 12','Week 13','Week 14','Week 15','Week 16','Week 17','Week 18','Week 19','Week 20']
+
+var H_id = [['H1_1','H1_2','H1_3'],['H2_1','H2_2','H2_3'],['H3_1','H3_2','H3_3'],['H4_1','H4_2','H4_3'],['H5_1','H5_2','H5_3'],['H6_1','H6_2','H6_3']]
+var h_id = [['h1_1','h1_2','h1_3'],['h2_1','h2_2','h2_3'],['h3_1','h3_2','h3_3'],['h4_1','h4_2','h4_3'],['h5_1','h5_2','h5_3'],['h6_1','h6_2','h6_3']]
+
+var P_id = [['P1_1','P1_2','P1_3'],['P2_1','P2_2','P2_3'],['P3_1','P3_2','P3_3'],['P4_1','P4_2','P4_3'],['P5_1','P5_2','P5_3'],['P6_1','P6_2','P6_3']]
+var p_id = [['p1_1','p1_2','p1_3'],['p2_1','p2_2','p2_3'],['p3_1','p3_2','p3_3'],['p4_1','p4_2','p4_3'],['p5_1','p5_2','p5_3'],['p6_1','p6_2','p6_3']]
+
+var place_color = ['rgba(212,175,55,.45)','rgba(192,192,192,.45)','rgba(205,127,50,.45)']
+
+var medal_teams = ['1_img','2_img','3_img','4_img','5_img','6_img','7_img','8_img','9_img','10_img','11_img','12_img','13_img','14_img']
+
+var medal_id = [['g1','s1','b1'],['g2','s2','b2'],['g3','s3','b3'],['g4','s4','b4'],['g5','s5','b5'],['g6','s6','b6'],['g7','s7','b7'],
+				['g8','s8','b8'],['g9','s9','b9'],['g10','s10','b10'],['g11','s11','b11'],['g12','s12','b12'],['g13','s13','b13'],['g14','s14','b14']]
+
+var medal_PH = [['1_h','1_p'],['2_h','2_p'],['3_h','3_p'],['4_h','4_p'],['5_h','5_p'],['6_h','6_p'],['7_h','7_p'],
+				['8_h','8_p'],['9_h','9_p'],['10_h','10_p'],['11_h','11_p'],['12_h','12_p'],['13_h','13_p'],['14_h','14_p']]
+
+var H_array = CreateTextId('H')
+var P_array = CreateTextId('P')
+
+var AllWeeks
+var AllPlaces
+var week
+var Medals
+var medals_ranked
+
+setTimeout(function(){ 
+	week = GetWeekNumber_L()
+	AllWeeks = GetAllWeeks_L();
+	AllPlaces = GetAllPlaces_L();
+	Medals = GetMedalCount();
+	Medals_R = GetMedalRank();
+	DisplaySeasonLeader();
+	DisplayMedalCount(Medals_R,Medals)}, 550)
+
+
+// Defining Functions
+//-----------------------------------------------------------------
+
+function Text_Display(lead,h_tag,p_tag){
+	for(var i = 0; i < 24; i++){
+		for(var j = 0; j < 9 ;j++){
+			if(i < 12){
+				text_tag = h_tag[i][j]
+			}else{
+				text_tag = p_tag[i-12][j]
+			}
+			if( j < lead[i].length){
+				document.getElementById(text_tag).innerHTML = lead[i][j]
+			}
+			else{
+				document.getElementById(text_tag).innerHTML = '   '
+			}
+		}
+	}
+}
+
+function CreateTextId(letter){
+	var id_array = []
+	for(var i = 1; i < 7;i++){
+		for(var j = 1; j < 3;j++){
+			row = []
+			for(var k = 1; k < 10; k++){
+				row.push( 'text_'+letter+i.toString()+k.toString()+'_'+j.toString() )
+			}
+			id_array.push( row )
+		}
+	}
+	return id_array
+}
+
+function CreateDisplay(Places){
+	var total_length = 0
+	var total_top = 0
+	for(var i = 0; i < 12;i++){
+		total_length = 0
+		total_top = 0
+		for(var j = 0; j < 3; j++){
+			length = Places[i][j]
+			top_px = total_top.toString()+'px'
+			total_length = total_length + length
+			cell_height = (length*36).toString()+'px'
+			total_top = total_top + length*36
+			if(i < 6){
+				cell_tag = H_id[i][j]
+				img_tag = h_id[i][j]
+				document.getElementById(cell_tag).style.left = Leader_Left[i]
+			}else{
+				cell_tag = P_id[i-6][j]
+				img_tag = p_id[i-6][j]
+				document.getElementById(cell_tag).style.left = Leader_Left[i-6]
+			}
+			cell_child_tag = cell_tag+'_color'
+			document.getElementById(cell_tag).style.height = cell_height
+			document.getElementById(cell_tag).style.top = top_px
+			document.getElementById(cell_child_tag).style.position = 'absolute'
+			document.getElementById(cell_child_tag).style.height = '100%'
+			document.getElementById(cell_child_tag).style.width = '100%'
+			if( length !== 0){
+				document.getElementById(cell_child_tag).style.background = place_color[j]
+				document.getElementById(img_tag).src = Leader_Pics[length-1]
+				document.getElementById(cell_tag).style.border='2px solid black';
+			}else{
+				document.getElementById(cell_child_tag).style.background = 'rgba(212,175,55,0)'
+				document.getElementById(img_tag).src = '../Team Images/Blank Pic.png'
+				document.getElementById(cell_tag).style.border='0px solid black';
+			}
+		}
+	}
+}
+
+function DisplayMedalCount(rank,medals){
+	console.log(rank,medals)
+	for(var i = 0; i < 14; i++){
+		r = rank[0][i]
+		document.getElementById(medal_teams[i]).src='../Team Images/'+TeamPics[r]
+		document.getElementById(medal_id[i][0]).innerHTML = medals[i][0]
+		document.getElementById(medal_id[i][1]).innerHTML = medals[i][1]
+		document.getElementById(medal_id[i][2]).innerHTML = medals[i][2]
+		document.getElementById(medal_PH[i][0]).innerHTML = medals[i][3]
+		document.getElementById(medal_PH[i][1]).innerHTML = medals[i][4]
+	}
+}
+
+function DisplayWeekLeader(index){
+	if(index <= week){
+		w = index - 1
+	Text_Display(AllWeeks_L[w],H_array,P_array);
+	CreateDisplay(AllPlaces[w])
+	document.getElementById('Leader_title').innerHTML = Week_Titles[w]
+	}else{
+		DisplaySeasonLeader()
+	}
+}
+
+function DisplaySeasonLeader(){
+	w = week
+	Text_Display(AllWeeks_L[w],H_array,P_array);
+	CreateDisplay(AllPlaces[w])
+	document.getElementById('Leader_title').innerHTML = "Season"
+}
+
---
+++ Season_22/Leaders/Read_Leaders.js
@@ -0,0 +1,378 @@
+var Week1_L = []
+var Week2_L = []
+var Week3_L = []
+var Week4_L = []
+var Week5_L = []
+var Week6_L = []
+var Week7_L = []
+var Week8_L = []
+var Week9_L = []
+var Week10_L = []
+var Week11_L = []
+var Week12_L = []
+var Week13_L = []
+var Week14_L = []
+var Week15_L = []
+var Week16_L = []
+var Week17_L = []
+var Week18_L = []
+var Week19_L = []
+var Week20_L = []
+var Season_L = []
+
+var Week1_Places = []
+var Week2_Places = []
+var Week3_Places = []
+var Week4_Places = []
+var Week5_Places = []
+var Week6_Places = []
+var Week7_Places = []
+var Week8_Places = []
+var Week9_Places = []
+var Week10_Places = []
+var Week11_Places = []
+var Week12_Places = []
+var Week13_Places = []
+var Week14_Places = []
+var Week15_Places = []
+var Week16_Places = []
+var Week17_Places = []
+var Week18_Places = []
+var Week19_Places = []
+var Week20_Places = []
+var Season_Places = []
+
+var Medal_Count = []
+var Medal_Rank = []
+
+//  Setting all the Initial Components
+//----------------------------------------------------------------
+
+var CSV_Names_L = ["week_1_leaders.csv","week_2_leaders.csv","week_3_leaders.csv","week_4_leaders.csv","week_5_leaders.csv",
+				   "week_6_leaders.csv","week_7_leaders.csv","week_8_leaders.csv","week_9_leaders.csv","week_10_leaders.csv",
+				   "week_11_leaders.csv","week_12_leaders.csv","week_13_leaders.csv","week_14_leaders.csv","week_15_leaders.csv",
+				   "week_16_leaders.csv","week_17_leaders.csv","week_18_leaders.csv","week_19_leaders.csv","week_20_leaders.csv","season_leaders.csv"]
+
+var Names = ['Will','Clint','Brian','Daniel','Mike','Cullen','Ryan','Charlie','Allen','Josh','Matt','McD','Nate','Tim']
+
+
+var AllWeeks_L
+var AllPlaces_L
+var MedalCount
+
+var week = 99
+ParseWeek( CollectWeek,week )
+ParseMedals( CollectMedals,Medal_Count,Medal_Rank)
+
+console.log(Medal_Count)
+console.log(Medal_Rank)
+
+setTimeout(function(){ 
+	ParseAll_L(week,CSV_Names_L);},60)
+
+setTimeout(function(){ 	
+	AllWeeks_L = CombineAllWeeks_L(week);
+	AllPlaces_L = CombineAllPlaces_L(week);},160)
+
+// Defining Functions
+//-----------------------------------------------------------------
+
+function ParseWeek( CallBack,int ){
+	Papa.parse('../CSV Files/Week.csv', {
+		download: true,
+		header: false,
+		dynamicTyping: true,
+		complete: function(results) {
+			CallBack(results.data,int)
+		}
+	})
+}
+
+function ParseMedals( CallBack,mat,vec ){
+	Papa.parse('../CSV Files/medal_count.csv', {
+		download: true,
+		header: false,
+		dynamicTyping: true,
+		complete: function(results) {
+			CallBack(results.data,mat,vec)
+		}
+	})
+}
+
+function CollectWeek(data,w){
+	w = data[0][0]
+	week = data[0][0]
+}
+
+function CollectMedals(data,mat,vec){
+	for( var i = 0; i < 15; i++ ){
+		if( i < 14 ){
+			mat.push( data[i] )
+		}
+		else{
+			vec.push( data[i] )
+		}
+	}
+}
+
+function GetAllWeeks_L(){
+	return AllWeeks_L
+}
+
+function GetAllPlaces_L(){
+	return AllPlaces_L
+}
+
+function ParseAll_L( w,names ){
+	for( var i = 0; i < w; i++ ){
+		NowParse_L(i,names)
+	}
+	NowParse_L(20,names)
+}
+
+function ParseData_L( CallBack, file, Week_Mat, Place_Mat){
+	Papa.parse(file, {
+		download: true,
+		header: false,
+		dynamicTyping: true,
+		complete: function(results) {
+			CallBack(results.data,Week_Mat,Place_Mat)
+		}
+	})
+}
+
+function CollectData_L( data, mat1, mat2 ) {
+	for (var i = 0; i < 36; i++) {
+		if( i < 24 ){
+			mat1.push( data[i] )
+		}
+		else{
+			mat2.push( data[i] )
+		}
+	}
+}
+
+function GetWeekNumber_L(){
+	return week
+}
+
+function GetMedalCount(){
+	return Medal_Count
+}
+
+function GetMedalRank(){
+	return Medal_Rank
+}
+
+function DetermineTeam(name){
+	for(var i = 0; i < 14; i++){
+		if( name === Names[i] ){
+			return i
+		}
+	}
+}
+
+function NowParse_L(index,file) {
+	name = file[index]
+	filename = '../CSV Files/'+name
+	if(index === 0){
+		ParseData_L( CollectData_L,filename,Week1_L,Week1_Places)
+	}
+	if(index === 1){
+		ParseData_L( CollectData_L,filename,Week2_L,Week2_Places)
+	}
+	if(index === 2){
+		ParseData_L( CollectData_L,filename,Week3_L,Week3_Places)
+	}
+	if(index === 3){
+		ParseData_L( CollectData_L,filename,Week4_L,Week4_Places)
+	}
+	if(index === 4){
+		ParseData_L( CollectData_L,filename,Week5_L,Week5_Places)
+	}	
+	if(index === 5){
+		ParseData_L( CollectData_L,filename,Week6_L,Week6_Places)
+	}
+	if(index === 6){
+		ParseData_L( CollectData_L,filename,Week7_L,Week7_Places)
+	}
+	if(index === 7){
+		ParseData_L( CollectData_L,filename,Week8_L,Week8_Places)
+	}
+	if(index === 8){
+		ParseData_L( CollectData_L,filename,Week9_L,Week9_Places)
+	}
+	if(index === 9){
+		ParseData_L( CollectData_L,filename,Week10_L,Week10_Places)
+	}
+	if(index === 10){
+		ParseData_L( CollectData_L,filename,Week11_L,Week11_Places)
+	}
+	if(index === 11){
+		ParseData_L( CollectData_L,filename,Week12_L,Week12_Places)
+	}
+	if(index === 12){
+		ParseData_L( CollectData_L,filename,Week13_L,Week13_Places)
+	}
+	if(index === 13){
+		ParseData_L( CollectData_L,filename,Week14_L,Week14_Places)
+	}
+	if(index === 14){
+		ParseData_L( CollectData_L,filename,Week15_L,Week15_Places)
+	}	
+	if(index === 15){
+		ParseData_L( CollectData_L,filename,Week16_L,Week16_Places)
+	}
+	if(index === 16){
+		ParseData_L( CollectData_L,filename,Week17_L,Week17_Places)
+	}
+	if(index === 17){
+		ParseData_L( CollectData_L,filename,Week18_L,Week18_Places)
+	}
+	if(index === 18){
+		ParseData_L( CollectData_L,filename,Week19_L,Week19_Places)
+	}
+	if(index === 19){
+		ParseData_L( CollectData_L,filename,Week20_L,Week20_Places)
+	}
+	if(index === 20){
+		ParseData_L( CollectData_L,filename,Season_L,Season_Places)
+	}
+	
+}
+
+function CombineAllWeeks_L(w){
+	var AllWeeks = []
+	for( var i = 0; i < w ;i++ ){
+		if(i==0){
+			AllWeeks.push(Week1_L)
+		}
+		if(i==1){
+			AllWeeks.push(Week2_L)
+		}
+		if(i==2){
+			AllWeeks.push(Week3_L)
+		}
+		if(i==3){
+			AllWeeks.push(Week4_L)
+		}
+		if(i==4){
+			AllWeeks.push(Week5_L)
+		}
+		if(i==5){
+			AllWeeks.push(Week6_L)
+		}
+		if(i==6){
+			AllWeeks.push(Week7_L)
+		}
+		if(i==7){
+			AllWeeks.push(Week8_L)
+		}
+		if(i==8){
+			AllWeeks.push(Week9_L)
+		}
+		if(i==9){
+			AllWeeks.push(Week10_L)
+		}
+		if(i==10){
+			AllWeeks.push(Week11_L)
+		}
+		if(i==11){
+			AllWeeks.push(Week12_L)
+		}
+		if(i==12){
+			AllWeeks.push(Week13_L)
+		}
+		if(i==13){
+			AllWeeks.push(Week14_L)
+		}
+		if(i==14){
+			AllWeeks.push(Week15_L)
+		}
+		if(i==15){
+			AllWeeks.push(Week16_L)
+		}
+		if(i==16){
+			AllWeeks.push(Week17_L)
+		}
+		if(i==17){
+			AllWeeks.push(Week18_L)
+		}
+		if(i==18){
+			AllWeeks.push(Week19_L)
+		}
+		if(i==19){
+			AllWeeks.push(Week20_L)
+		}
+	}
+	AllWeeks.push(Season_L)
+	return AllWeeks
+}
+
+function CombineAllPlaces_L(week){
+	var AllPlaces = []
+	for( var i = 0; i < week ;i++ ){
+		if(i==0){
+			AllPlaces.push(Week1_Places)
+		}
+		if(i==1){
+			AllPlaces.push(Week2_Places)
+		}
+		if(i==2){
+			AllPlaces.push(Week3_Places)
+		}
+		if(i==3){
+			AllPlaces.push(Week4_Places)
+		}
+		if(i==4){
+			AllPlaces.push(Week5_Places)
+		}
+		if(i==5){
+			AllPlaces.push(Week6_Places)
+		}
+		if(i==6){
+			AllPlaces.push(Week7_Places)
+		}
+		if(i==7){
+			AllPlaces.push(Week8_Places)
+		}
+		if(i==8){
+			AllPlaces.push(Week9_Places)
+		}
+		if(i==9){
+			AllPlaces.push(Week10_Places)
+		}
+		if(i==10){
+			AllPlaces.push(Week11_Places)
+		}
+		if(i==11){
+			AllPlaces.push(Week12_Places)
+		}
+		if(i==12){
+			AllPlaces.push(Week13_Places)
+		}
+		if(i==13){
+			AllPlaces.push(Week14_Places)
+		}
+		if(i==14){
+			AllPlaces.push(Week15_Places)
+		}
+		if(i==15){
+			AllPlaces.push(Week16_Places)
+		}
+		if(i==16){
+			AllPlaces.push(Week17_Places)
+		}
+		if(i==17){
+			AllPlaces.push(Week18_Places)
+		}
+		if(i==18){
+			AllPlaces.push(Week19_Places)
+		}
+		if(i==19){
+			AllPlaces.push(Week20_Places)
+		}
+	}
+	AllPlaces.push(Season_Places)
+	return AllPlaces
+}
